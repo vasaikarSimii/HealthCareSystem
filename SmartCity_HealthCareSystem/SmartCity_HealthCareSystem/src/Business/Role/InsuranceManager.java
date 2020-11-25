05218f9f5c5eb3beefaa723787581a7be09c48cf
jatinsharma1 <sharma.jat@northeastern.edu>
2020-11-25 09:25:38 -0500

    Prototype and UI

---
+++ SmartCity_HealthCareSystem/SmartCity_HealthCareSystem/src/Business/Role/InsuranceManager.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Role;
+
+import Business.EcoSystem;
+import Business.Enterprise.Enterprise;
+import Business.Organization.Doctor;
+import Business.Organization.Organization;
+import Business.UserAccount.UserAccount;
+//import userinterface.DoctorRole.DoctorWorkAreaJPanel;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author seemin
+ */
+public class InsuranceManager extends Role{
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business) {
+        //remove null
+        return null;
+        //return new DoctorWorkAreaJPanel(userProcessContainer, account, (Doctor)organization, enterprise);
+    }
+    
+    
+}
