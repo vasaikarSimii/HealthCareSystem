6a3e7cbf27177809b29586b97dd4914c331e6cf5
vasaikarSimii <vasaikar.s@northeastern.edu>
2020-11-25 23:48:10 +0530

    CardLayoutCodes

--- SmartCity_HealthCareSystem/SmartCity_HealthCareSystem/src/userinterface/MedicalServicesEnterprise/CreatePharmacyJPanel.java
+++ SmartCity_HealthCareSystem/SmartCity_HealthCareSystem/src/userinterface/MedicalServicesEnterprise/CreatePharmacyJPanel.java
@@ -5,17 +5,24 @@
  */
 package userinterface.MedicalServicesEnterprise;
 
+import Business.EcoSystem;
+import javax.swing.JPanel;
+
 /**
  *
  * @author jshar
  */
 public class CreatePharmacyJPanel extends javax.swing.JPanel {
 
+    private JPanel rightPanel;
+    private EcoSystem ecosystem;
     /**
      * Creates new form CreatePharmacy
      */
-    public CreatePharmacyJPanel() {
+    public CreatePharmacyJPanel(JPanel rightPanel, EcoSystem ecosystem) {
         initComponents();
+        this.rightPanel = rightPanel;
+        this.ecosystem = ecosystem;
     }
 
     /**
@@ -27,15 +34,17 @@ public class CreatePharmacyJPanel extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        setBackground(new java.awt.Color(255, 255, 255));
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 1345, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 700, Short.MAX_VALUE)
         );
     }// </editor-fold>//GEN-END:initComponents
 
