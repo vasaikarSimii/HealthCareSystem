d748b144470f7c8e150bc62360e679b0da0f7042
jatinsharma1 <sharma.jat@northeastern.edu>
2020-12-14 19:31:57 -0500

    NetworkEnterprise

--- SmartCity_HealthCareSystem/SmartCity_HealthCareSystem/src/userinterface/SystemAdmin/ManageNetworkJPanel.java
+++ SmartCity_HealthCareSystem/SmartCity_HealthCareSystem/src/userinterface/SystemAdmin/ManageNetworkJPanel.java
@@ -140,7 +140,7 @@ public class ManageNetworkJPanel extends javax.swing.JPanel {
         add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 94, 541, 112));
 
         jLabel1.setText("City Name:");
-        add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 240, -1, -1));
+        add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 240, -1, -1));
         add(txtStateName, new org.netbeans.lib.awtextra.AbsoluteConstraints(339, 281, 165, -1));
 
         submitJButton.setText("Submit");
